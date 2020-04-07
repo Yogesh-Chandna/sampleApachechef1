1818a944a5fedd0956abb3588352125db717c327
yogeshchandna <59799135+Yogesh-Chandna@users.noreply.github.com>
2020-04-07 19:57:34 +0530

    Update default.rb

--- web/recipes/default.rb
+++ web/recipes/default.rb
@@ -3,7 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+package "httpd"
 
+service "httpd" do
+	action :start
+end
 
 file "/var/www/html/index.html" do
 	content "Hello Yogesh"
